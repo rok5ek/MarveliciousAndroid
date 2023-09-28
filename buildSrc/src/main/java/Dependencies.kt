38e152e92c1255c45d3cd2aac35b1cd04f30fba6
Rok Petek <rok.petek@bitstamp.net>
2023-09-28 13:15:40 +0200

    Added kotlin buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,9 @@
+object Versions {
+    val androidCoreKtx = "1.12.0"
+    val retrofit = "2.3.0"
+    val rxjava = "2.1.9"
+}
+
+object Dependencies {
+    val androidCoreKtx = "androidx.core:core-ktx:${Versions.androidCoreKtx}"
+}
